b9e7c5e162319d92e43294f7a5d514471dec98f9
LenaSpivakova <Skarlet2710@mail.ru>
2025-05-18 00:20:58 +0300

    Класс Util должен содержать логику настройки соединения с базой данных

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,19 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:postgresql://localhost:5432/tutdb";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "S2002Le";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Ошибка подключения к базе данных SQL: ", e);
+        }
+    }
 }
